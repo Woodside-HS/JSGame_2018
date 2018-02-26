c37ec7dd72b54ac5189fb9ca571893ce56f65cae
Eric Ettlin <eettlin@seq.org>
2018-02-26 12:56:57 -0800

    comments
    add comments

--- game/res/js/map/mapmanager.js
+++ game/res/js/map/mapmanager.js
@@ -5,13 +5,16 @@ class MapManager extends Updateable{
     this.game = game;
     this.validStartTiles = [];
   }
+  //  add comments here
   init(){
+    //  add comments here
     for(let i = 0; i < CONFIG.MAP_X_SIZE; i++){ // columns of rows
       this.map.push([]);
       for(let j = 0; j < CONFIG.MAP_Y_SIZE; j++){
         this.map[i].push(new Tile(this.game, new Vector2D(i, j)));
       }
     }
+    //  add comments here
     for(let i = 0; i < CONFIG.MAP_X_SIZE; i++){ // columns of rows
       for(let j = 0; j < CONFIG.MAP_Y_SIZE; j++){
         this.map[i][j].init();
@@ -19,14 +22,18 @@ class MapManager extends Updateable{
     }
     this.game.canvas.addEventListener("click",this.onClick);
   }
+  //  add comments here
   update(){
+    //  add comments here
     for(let i = 0; i < CONFIG.MAP_X_SIZE; i++){
       for(let j = 0; j < CONFIG.MAP_Y_SIZE; j++){
         this.map[i][j].update();
       }
     }
   }
+  //  add comments here
   render(){
+    //  add comments here
     for(let i = 0; i < CONFIG.MAP_X_SIZE; i++){
       for(let j = 0; j < CONFIG.MAP_Y_SIZE; j++){
         this.map[i][j].render();
