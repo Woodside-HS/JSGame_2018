4039ab474810b437a3ca27b297a4826455950f4b
ian-ramsey <ianrsk8s@gmail.com>
2018-03-06 11:46:28 -0800

    a little refactoring

--- public-html/res/js/player/player.js
+++ public-html/res/js/player/player.js
@@ -21,11 +21,33 @@
       this.v.upComps();
     }
      this.loc.add(this.v);
+     let vDir=this.v.duplicate();
+     vDir.m=PLAYER_CONFIG.SIZE/2;
+     vDir.upComps();
+     let hitBoxPos = this.loc.duplicate();
+     hitBoxPos.add(vDir);
+     let cloc=positionToGrid(hitBoxPos);
+     if(cloc.x<0||cloc.x>=CONFIG.MAP_X_SIZE||cloc.y<0||cloc.y>=CONFIG.MAP_X_SIZE||
+       this.game.mapManager.map[cloc.x][cloc.y].tileType.IS_OCCUPIED){
+       this.loc.subtract(this.v);
+    //   this.v.multiply(-.5);
+       this.v.multiply(0);
+       this.loc.add(this.v);
+     }
      this.v.multiply(.9);
-
    }
    render() {
+     //for debugging purposes
+     if(CONFIG.DEBUG_MODE){
+     let vDir=this.v.duplicate();
+     vDir.m=PLAYER_CONFIG.SIZE/2;
+     vDir.upComps();
+     vDir.add(this.loc);
      this.game.context.fillStyle = this.fillStyle;
-     this.game.context.fillRect(this.loc.x, this.loc.y, PLAYER_CONFIG.SIZE, PLAYER_CONFIG.SIZE);
+     this.game.context.fillRect(vDir.x-PLAYER_CONFIG.SIZE/6, vDir.y-PLAYER_CONFIG.SIZE/6, PLAYER_CONFIG.SIZE/3, PLAYER_CONFIG.SIZE/3);
+    }
+
+     this.game.context.fillStyle = this.fillStyle;
+     this.game.context.fillRect(this.loc.x-PLAYER_CONFIG.SIZE/2, this.loc.y-PLAYER_CONFIG.SIZE/2, PLAYER_CONFIG.SIZE, PLAYER_CONFIG.SIZE);
    }
  }
