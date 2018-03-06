ba0a59abbe1f024ef03f2f78456415d2ca40cc85
claytonwramsey <claytonwramsey@gmail.com>
2018-03-06 12:33:01 -0800

    Added border to minimap

--- public-html/res/js/ui/minimap.js
+++ public-html/res/js/ui/minimap.js
@@ -13,6 +13,12 @@ class Minimap extends Updateable {
     let xContentSize = CONFIG.MAP_X_SIZE * UI_CONFIG.MINIMAP_TILE_SIZE;
     let yContentSize = CONFIG.MAP_Y_SIZE * UI_CONFIG.MINIMAP_TILE_SIZE;
     let startCoordinate = new Vector2D(this.game.canvas.width - xContentSize, this.game.canvas.height - yContentSize);
+    this.game.context.fillStyle = UI_CONFIG.MINIMAP_BORDER_COLOR;
+    this.game.context.fillRect(
+            startCoordinate.x-UI_CONFIG.MINIMAP_BORDER_STROKE,
+            startCoordinate.y-UI_CONFIG.MINIMAP_BORDER_STROKE,
+            xContentSize+UI_CONFIG.MINIMAP_BORDER_STROKE,
+            yContentSize+UI_CONFIG.MINIMAP_BORDER_STROKE);
     for (let i = 0; i < this.game.mapManager.map.length; i++) {
       for (let j = 0; j < this.game.mapManager.map[i].length; j++) {
         this.game.context.fillStyle = this.game.mapManager.map[i][j].tileType.MINIMAP_COLOR;
