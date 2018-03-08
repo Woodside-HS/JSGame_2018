ebae36c7caea0c4d8b087df68b4e712d2bc2aa35
Aetaris <jeremiah.starborn@gmail.com>
2018-03-08 14:21:29 -0800

    Issues #1, #2
    Partial implementation of issues

--- public-html/OuterGame/1.11 luarals Game Code/rocketship.js
+++ public-html/OuterGame/1.11 luarals Game Code/rocketship.js
@@ -2,8 +2,8 @@ class Rocketship{
 
   constructor(location){
     this.loc = location;
-    this.vel = new vector2d(0,0);
-    this.acc = new vector2d(0,0);
+    this.vel = new Vector2D(0,0);
+    this.acc = new Vector2D(0,0);
     //dir keeps track of direction ship is pointing when velocity is 0 or too small
     //ship starts pointing right
     this.dir = 0;
@@ -50,7 +50,7 @@ class Rocketship{
     } else{
       m = 0.00003;
     }
-    var f = new vector2d(undefined, undefined, m, this.dir + Math.PI/2);
+    var f = new Vector2D(undefined, undefined, m, this.dir + Math.PI/2);
     this.applyForce(f);
   }
 
@@ -62,13 +62,13 @@ class Rocketship{
     } else{
       m = 0.00003;
     }
-    var f = new vector2d(undefined, undefined, m, this.dir - Math.PI/2);
+    var f = new Vector2D(undefined, undefined, m, this.dir - Math.PI/2);
     this.applyForce(f);
   }
 
   accelerate(){
     //apply a force in the direction the ship is traveling
-    var f = new vector2d(undefined, undefined, 0.1, this.dir);
+    var f = new Vector2D(undefined, undefined, 0.1, this.dir);
     this.applyForce(f);
   }
 
@@ -76,7 +76,7 @@ class Rocketship{
     //apply a force in the opposite direction of the ship
     //checks velocity to make ship stop, not travel backward
     if(this.vel.magnitude() > 0.0001){
-      var f = new vector2d(undefined, undefined, this.vel.magnitude()/30, this.vel.theta() + Math.PI);
+      var f = new Vector2D(undefined, undefined, this.vel.magnitude()/30, this.vel.theta() + Math.PI);
       this.applyForce(f);
     }
   }
