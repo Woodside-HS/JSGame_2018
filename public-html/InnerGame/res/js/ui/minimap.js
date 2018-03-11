fa7126727932aa32c338873900b29d5c85a1a2f2
Loran Kary <lk@focalpnt.com>
2018-03-10 18:43:31 -0800

    everything from uiconfig.js

--- public-html/InnerGame/res/js/ui/minimap.js
+++ public-html/InnerGame/res/js/ui/minimap.js
@@ -10,33 +10,33 @@ class Minimap extends Updateable {
 
   }
   render() {
-    let xContentSize = config.map_x_size * UI_CONFIG.MINIMAP_TILE_SIZE;
-    let yContentSize = config.map_y_size * UI_CONFIG.MINIMAP_TILE_SIZE;
+    let xContentSize = config.map_x_size * ui_config.minimap_tile_size;
+    let yContentSize = config.map_y_size * ui_config.minimap_tile_size;
     let startCoordinate = new Vector2D(this.game.canvas.width - xContentSize, this.game.canvas.height - yContentSize);
-    this.game.context.fillStyle = UI_CONFIG.MINIMAP_BORDER_COLOR;
+    this.game.context.fillStyle = ui_config.minimap_border_color;
     this.game.context.fillRect(
-            startCoordinate.x-UI_CONFIG.MINIMAP_BORDER_STROKE,
-            startCoordinate.y-UI_CONFIG.MINIMAP_BORDER_STROKE,
-            xContentSize+UI_CONFIG.MINIMAP_BORDER_STROKE,
-            yContentSize+UI_CONFIG.MINIMAP_BORDER_STROKE);
+            startCoordinate.x-ui_config.minimap_border_stroke,
+            startCoordinate.y-ui_config.minimap_border_stroke,
+            xContentSize+ui_config.minimap_border_stroke,
+            yContentSize+ui_config.minimap_border_stroke);
     for (let i = 0; i < this.game.mapManager.map.length; i++) {
       for (let j = 0; j < this.game.mapManager.map[i].length; j++) {
         //draw the land
         this.game.context.fillStyle = this.game.mapManager.map[i][j].tileType.minimap_color;
         this.game.context.fillRect(
-                startCoordinate.x + UI_CONFIG.MINIMAP_TILE_SIZE * i,
-                startCoordinate.y + UI_CONFIG.MINIMAP_TILE_SIZE * j,
-                UI_CONFIG.MINIMAP_TILE_SIZE,
-                UI_CONFIG.MINIMAP_TILE_SIZE);
+                startCoordinate.x + ui_config.minimap_tile_size * i,
+                startCoordinate.y + ui_config.minimap_tile_size * j,
+                ui_config.minimap_tile_size,
+                ui_config.minimap_tile_size);
       }
     }
     
     //draw the player
     this.game.context.fillStyle = player_config.minimap_color;
     this.game.context.fillRect(
-            startCoordinate.x+UI_CONFIG.MINIMAP_TILE_SIZE*this.game.player.cloc.x,
-            startCoordinate.y+UI_CONFIG.MINIMAP_TILE_SIZE*this.game.player.cloc.y,
-            UI_CONFIG.MINIMAP_TILE_SIZE,
-            UI_CONFIG.MINIMAP_TILE_SIZE);
+            startCoordinate.x+ui_config.minimap_tile_size*this.game.player.cloc.x,
+            startCoordinate.y+ui_config.minimap_tile_size*this.game.player.cloc.y,
+            ui_config.minimap_tile_size,
+            ui_config.minimap_tile_size);
   }
 }
