74f017c82637f374a6658d8531a4307941588e71
claytonwramsey <claytonwramsey@gmail.com>
2018-03-12 11:39:25 -0700

    Added comments to utils.js

--- public-html/InnerGame/res/js/ui/minimap.js
+++ public-html/InnerGame/res/js/ui/minimap.js
@@ -12,15 +12,22 @@ class Minimap extends Updateable {
   render() {
     let xContentSize = config.map_x_size * ui_config.minimap_tile_size;
     let yContentSize = config.map_y_size * ui_config.minimap_tile_size;
-    let startCoordinate = new Vector2D(this.game.canvas.width - xContentSize, this.game.canvas.height - yContentSize);
+    let startCoordinate = new Vector2D(
+            this.game.canvas.width - xContentSize,
+            this.game.canvas.height - yContentSize);
+
+    //Draw border
     this.game.context.fillStyle = ui_config.minimap_border_color;
     this.game.context.fillRect(
-            startCoordinate.x-ui_config.minimap_border_stroke,
-            startCoordinate.y-ui_config.minimap_border_stroke,
-            xContentSize+ui_config.minimap_border_stroke,
-            yContentSize+ui_config.minimap_border_stroke);
+            startCoordinate.x - ui_config.minimap_border_stroke,
+            startCoordinate.y - ui_config.minimap_border_stroke,
+            xContentSize + ui_config.minimap_border_stroke,
+            yContentSize + ui_config.minimap_border_stroke);
+
+
     for (let i = 0; i < this.game.mapManager.map.length; i++) {
       for (let j = 0; j < this.game.mapManager.map[i].length; j++) {
+
         //draw the land
         this.game.context.fillStyle = this.game.mapManager.map[i][j].tileType.minimap_color;
         this.game.context.fillRect(
@@ -30,12 +37,12 @@ class Minimap extends Updateable {
                 ui_config.minimap_tile_size);
       }
     }
-    
+
     //draw the player
     this.game.context.fillStyle = player_config.minimap_color;
     this.game.context.fillRect(
-            startCoordinate.x+ui_config.minimap_tile_size*this.game.player.cloc.x,
-            startCoordinate.y+ui_config.minimap_tile_size*this.game.player.cloc.y,
+            startCoordinate.x + ui_config.minimap_tile_size * this.game.player.cloc.x,
+            startCoordinate.y + ui_config.minimap_tile_size * this.game.player.cloc.y,
             ui_config.minimap_tile_size,
             ui_config.minimap_tile_size);
   }
