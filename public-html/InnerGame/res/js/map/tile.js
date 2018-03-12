7b71b6167917f6cd07a93702453a130185cc29dc
claytonwramsey <claytonwramsey@gmail.com>
2018-03-12 12:09:24 -0700

    Implemented gridlines

--- public-html/InnerGame/res/js/map/tile.js
+++ public-html/InnerGame/res/js/map/tile.js
@@ -9,15 +9,25 @@ class Tile extends Updateable {
     this.dir = -1;
     this.isGoal = false;
     this.tileType = tile_types.nulltype;
-    this.image=new Image();
+    this.image = new Image();
   }
-  init(){
-    this.image.src=this.tileType.image_src;
-    this.isOccupied=this.tileType.is_occupied;
+  init() {
+    this.image.src = this.tileType.image_src;
+    this.isOccupied = this.tileType.is_occupied;
   }
   render() {
-    // this.game.context.fillStyle = this.tileType.minimap_color;
-    // this.game.context.fillRect(this.loc.x, this.loc.y, config.tile_size, config.tile_size);
-    this.game.context.drawImage(this.image,this.loc.x, this.loc.y, config.tile_size, config.tile_size);
-   }
+    if (tile_config.draw_gridlines) {
+      this.game.context.fillStyle = tile_config.gridline_color;
+      this.game.context.fillRect(this.loc.x, this.loc.y, config.tile_size, config.tile_size);
+      this.game.context.drawImage(
+              this.image,
+              this.loc.x + tile_config.gridline_stroke / 2,
+              this.loc.y + tile_config.gridline_stroke / 2,
+              config.tile_size - tile_config.gridline_stroke / 2,
+              config.tile_size - tile_config.gridline_stroke / 2
+              )
+    } else {
+      this.game.context.drawImage(this.image, this.loc.x, this.loc.y, config.tile_size, config.tile_size);
+    }
+  }
 }
