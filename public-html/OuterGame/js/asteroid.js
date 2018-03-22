76fe4388d52f37d394968c6a06b3ccc2638e2a91
Aetaris <jeremiah.starborn@gmail.com>
2018-03-22 14:50:04 -0700

    Change to asteroid functionality

--- public-html/OuterGame/js/asteroid.js
+++ public-html/OuterGame/js/asteroid.js
@@ -3,25 +3,22 @@ class Asteroid extends Mover {
 	constructor(loc, vel,acc,radius) {
 		super(loc,vel,acc,radius);
 
-		let stats = new StatBlock(this.radius); // Health based on radius
+		let stats = new StatBlock(1000, {killable: false}); // Health based on radius
 		stats.assign(this);
 
 		this.deathEvents.push(() => {
 			let collisionVisual = new BulletImpactVisual(this.loc.clone(), 'gray');
 			collisionVisual.maxRadius = this.radius * 2;
 			System().addVisual(collisionVisual);
-
-
-			if(this.radius <= 8) {
-				return;
-			}
-			let num = Math.round(Math.random()) + 2;
-			for(let i = 0; i < num; i++) {
-				let vel = new Vector2D(Math.random() * 2 - 1, Math.random() * 2 - 1).setMag(this.vel.magnitude());
-				let newAsteroid = new Asteroid(this.loc.clone().add(vel.scalarMult(1)), vel, new Vector2D(0,0), (Math.random() * 1 + 0.5) * this.radius / num);
-				System().addEntity(newAsteroid);
-			}
 		});
+	}
+
+	update() {
+		this.radius -= this.stats.damageTaken / 3;
+		this.stats.damageTaken = 0;
+		if(this.radius <= 5) {
+			this.kill();
+		}
 	}
 
 	render() {
