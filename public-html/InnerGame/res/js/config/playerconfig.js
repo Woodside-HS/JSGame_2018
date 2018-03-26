c0436731a9811ad5def24b89d0592e11c5d0848e
ian-ramsey <ianrsk8s@gmail.com>
2018-03-26 11:41:04 -0700

    removed scaling

--- public-html/InnerGame/res/js/config/playerconfig.js
+++ public-html/InnerGame/res/js/config/playerconfig.js
@@ -1,6 +1,6 @@
 const player_config = {
-  size: 5,//px
-  max_speed: 3,//px/frame
+  size: 10,//px
+  max_speed: 7,//px/frame
   minimap_color: "#FF0000",
   movement_loss: 0.9//amount of friction on movement, px/f^2
 };
