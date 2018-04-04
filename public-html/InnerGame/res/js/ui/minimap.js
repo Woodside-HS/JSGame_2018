f3f61216a1e313b0399280884f768ab202320ad7
claytonwramsey <claytonwramsey@gmail.com>
2018-04-04 11:52:32 -0700

    i am forced to do this. rip me

--- public-html/InnerGame/res/js/ui/minimap.js
+++ public-html/InnerGame/res/js/ui/minimap.js
@@ -2,49 +2,55 @@ class Minimap extends Updateable {
   constructor(game) {
     super();
     this.game = game;
+    this.contentSize = new FastVector(
+            config.map_x_size * ui_config.minimap_tile_size,
+            config.map_y_size * ui_config.minimap_tile_size
+            );
+    this.imgData = null;
+    this.startCoordinate = null;
   }
   init() {
-
+    this.startCoordinate = new FastVector(
+            this.game.canvas.width - this.contentSize.x,
+            this.game.canvas.height - this.contentSize.y);
+    this.imgData = this.game.context.createImageData(
+            this.contentSize.x,
+            this.contentSize.y);
   }
   update() {
 
   }
   render() {
-    let contentSize = new FastVector(
-            config.map_x_size * ui_config.minimap_tile_size,
-            config.map_y_size * ui_config.minimap_tile_size
-            );
-    let startCoordinate = new Vector2D(this.game.canvas.width - contentSize.x, this.game.canvas.height - contentSize.y);
     this.game.context.fillStyle = ui_config.minimap_border_color;
     this.game.context.fillRect(
-            startCoordinate.x - ui_config.minimap_border_stroke,
-            startCoordinate.y - ui_config.minimap_border_stroke,
-            contentSize.x + ui_config.minimap_border_stroke,
-            contentSize.y + ui_config.minimap_border_stroke);
+            this.startCoordinate.x - ui_config.minimap_border_stroke,
+            this.startCoordinate.y - ui_config.minimap_border_stroke,
+            this.contentSize.x + ui_config.minimap_border_stroke,
+            this.contentSize.y + ui_config.minimap_border_stroke);
     for (let i = 0; i < this.game.mapManager.map.length; i++) {
       for (let j = 0; j < this.game.mapManager.map[i].length; j++) {
         //draw the land
         this.game.context.fillStyle = this.game.mapManager.map[i][j].tileType.minimap_color;
         // or draw towers
-        if(this.game.mapManager.towermanager.towers[i][j]){
+        if (this.game.mapManager.towermanager.towers[i][j]) {
           this.game.context.fillStyle = tower_config.minimap_color;
         }
         //fill the tile
         this.game.context.fillRect(
-                startCoordinate.x + ui_config.minimap_tile_size * i,
-                startCoordinate.y + ui_config.minimap_tile_size * j,
+                this.startCoordinate.x + ui_config.minimap_tile_size * i,
+                this.startCoordinate.y + ui_config.minimap_tile_size * j,
                 ui_config.minimap_tile_size,
                 ui_config.minimap_tile_size);
-                
-        
+
+
       }
     }
 
     //draw the player
     this.game.context.fillStyle = player_config.minimap_color;
     this.game.context.fillRect(
-            startCoordinate.x + ui_config.minimap_tile_size * this.game.player.cloc.x,
-            startCoordinate.y + ui_config.minimap_tile_size * this.game.player.cloc.y,
+            this.startCoordinate.x + ui_config.minimap_tile_size * this.game.player.cloc.x,
+            this.startCoordinate.y + ui_config.minimap_tile_size * this.game.player.cloc.y,
             ui_config.minimap_tile_size,
             ui_config.minimap_tile_size);
   }
