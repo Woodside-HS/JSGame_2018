24e89a306bc1b606a5f58a6aaff4524c03667946
ArianaPark <792913@seq.org>
2018-04-05 00:17:42 -0700

    issue 54, remove shopitem class, work on info div

--- public-html/OuterGame/js/spacestation.js
+++ public-html/OuterGame/js/spacestation.js
@@ -3,15 +3,23 @@ class SpaceStation{
   constructor(loc){
     this.loc = loc;
 
-    this.shopItems = []; //this stores all the items being solid, info used for buttons and info
-    this.loadShopItems(); //need to fix the function first since changed to html
-
     this.canLandOn = false;
 
-    this.infoDiv = { //prob dont need??? <<<<<<<<<<
-      item: null,
-      render : function(){
-        // console.log(this.item);
+    this.infoDiv = { //for changing the item shown in info div
+      info : document.getElementById("info"),
+      render : function(item){ //item=shop item div
+        this.removeChildren();
+        var img = item.children[0];
+        img.class = "infoImg";
+        var node = document.createTextNode(""+item.id);
+        node.class = "infoName";
+        info.appendChild(node);
+        info.appendChild(img);
+      },
+      removeChildren : function(){
+        for(let i=2;i<info.children.length;i++){
+          info.removeChild(info.children[i]);
+        }
       },
     };
 
@@ -22,11 +30,13 @@ class SpaceStation{
     //create an attribute of the space station class to add html in this file
     this.div.innerHTML = SpaceStation.html;
 
-    //event listener for clicking on button
+    //event listener for clicking on button to exit station back to outer game
     document.getElementById("exitButton").addEventListener("click",function(event){
       this.parentElement.style.display = "none";
       gameState = "outer";
     });
+
+    this.loadShopItems();
 
   }
 
@@ -41,44 +51,19 @@ class SpaceStation{
   }
 
   loadShopItems(){
-    //0
-    var img0 = document.createElement("img");
-    img0.src = "shopIMGS/0cookie.png";
-    this.shopItems[0] = new ShopItem("Cookie",img0,"1.55",true);
-    //1
-    var img1 = document.createElement("img");
-    img1.src = "shopIMGS/1brownie.png";
-    this.shopItems[1] = new ShopItem("Brownie",img1,"2.35",true);
-    //2
-    var img2 = document.createElement("img");
-    img2.src = "shopIMGS/2cupcake.png";
-    this.shopItems[2] = new ShopItem("Cupcake",img2,"3.10",true);
-    //3
-    var img3 = document.createElement("img");
-    img3.src = "shopIMGS/3pie.png";
-    this.shopItems[3] = new ShopItem("Pie",img3,"2.10",true);
-    //4
-    var img4 = document.createElement("img");
-    img4.src = "shopIMGS/4coffee.png";
-    this.shopItems[4] = new ShopItem("Coffee",img4,"4.75",true);
-
-    //draw button tiles for each shop item and show info on right
-    for(let i in this.shopItems){
-      let x = 120*(i%3) +30;
-      let y = 150*Math.floor(i/3) +50;
-      let button = document.createElement("div");
-      button.station = this;
-      button.className = "tile";
-      button.style.top = ""+y+"px";
-      button.style.left = ""+x+"px";
-      let img = document.createElement("img");
-      img.className = "img";
-      img.src = this.shopItems[i].image.src;
-      button.appendChild(img);
-      this.div.children[1].appendChild(button);
+    //draw button tiles for each shop item and add click listener to each button
+    var items = this.div.children[3]; //div is space station, getting items div
+    for(let i=2;i<items.children.length;i++){
+      let x = 120*((i-2)%3) +30;
+      let y = 150*Math.floor((i-2)/3) +100;
+      let button = items.children[i]; //div for a shop item
+      button.spacestation = this;
+      button.style.top = "" + y + "px";
+      button.style.left = "" + x + "px";
 
       button.addEventListener("click",function(event){
-        console.log(this.station.shopItems[i].name);
+        this.spacestation.infoDiv.render(this);
+        //render info of this item in the info div
       });
     }
   }
@@ -89,24 +74,26 @@ SpaceStation.html = '\
   <img src="shopIMGS/background.png" style="width:100%; height:100%;">\
   <img id="exitButton" src="shopIMGS/exit.png">\
   <div id="items">\
-    <img class="img" src="shopIMGS/shopBackground.png">\
-    <div class="tile">\
+    <h2 style="position:absolute; left:100px;">Items</h2>\
+    <img class="back" src="shopIMGS/shopBackground.png">\
+    <div id="Cookie" class="tile">\
+      <img class="img" src="shopIMGS/0cookie.png">\
+    </div>\
+    <div id="Brownie" class="tile">\
+      <img class="img" src="shopIMGS/1brownie.png">\
+    </div>\
+    <div id="Cupcake" class="tile">\
+      <img class="img" src="shopIMGS/2cupcake.png">\
+    </div>\
+    <div id="Coffee" class="tile">\
+      <img class="img" src="shopIMGS/4coffee.png">\
+    </div>\
+    <div id="Pie" class="tile">\
+      <img class="img" src="shopIMGS/3pie.png">\
     </div>\
   </div>\
   <div id="info">\
-    <img class="img" src="shopIMGS/infoBackground.png">\
+    <h2 style="position:absolute; left:100px;">Info</h2>\
+    <img class="back" src="shopIMGS/infoBackground.png">\
   </div>\
 ';
-
-class ShopItem{
-  //each item needs its own section
-  //each section should have:button to buy/sell, image, name, price
-
-  constructor(name,image,price,type){
-    this.name = name; //string
-    this.image = image; //image
-    this.price = price; //number
-    this.type = type; //boolean, true-buy, false-sell
-  }
-
-}
