b3ef7ea169747976abde46d695895927e3cc1ef9
ian-ramsey <ianrsk8s@gmail.com>
2018-05-09 12:33:13 -0700

    making it pretty

--- public-html/InnerGame/res/js/map/mapmanager.js
+++ public-html/InnerGame/res/js/map/mapmanager.js
@@ -140,9 +140,9 @@ class MapManager extends Updateable {
     }
     this.game.player.loc = startTile.loc.duplicate();
     let cloc = positionToGrid(this.game.player.loc);
-    for(let i=cloc.x-1; i<=cloc.x+1; i++)
-      for(let j=cloc.y-1; j<=cloc.y+1; j++)
-        if(i>0&&i<config.map_x_size&&j>0&&j<config.map_y_size)
+    for(let i=cloc.x-2; i<=cloc.x+2; i++)
+      for(let j=cloc.y-2; j<=cloc.y+2; j++)
+        if((Math.abs(i-cloc.x)+Math.abs(j-cloc.y))<=2&&i>=0&&i<config.map_x_size&&j>=0&&j<config.map_y_size)
           this.game.mapManager.map[i][j].seen = true;
     if(playerStats.revealLevel == 1 || playerStats.revealLevel == 2){
       this.game.player.revealCone();
