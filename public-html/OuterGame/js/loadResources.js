6e7716162d96f85f6e8e66512483af8daee1ceda
SethReis <795091@seq.org>
2018-05-11 12:16:46 -0700

    Implemented sounds and images: WARNING: EXTREAMLY LONG LOAD TIMES

--- public-html/OuterGame/js/loadResources.js
+++ public-html/OuterGame/js/loadResources.js
@@ -85,6 +85,139 @@ let imageSources = {//Properties of imageSources match names of images
 	splash: {
 		sourceString:"../InnerGame/res/sprites/splash/youdied.jpg",
 	},
+	alien: {
+		sourceString:"../InnerGame/res/sprites/creatures/Alien.png",
+	},
+	bunny: {
+		sourceString:"../InnerGame/res/sprites/creatures/Bunny.png",
+	},
+	creep: {
+		sourceString:"../InnerGame/res/sprites/creatures/Creep.png",
+	},
+	fireSpider: {
+		sourceString:"../InnerGame/res/sprites/creatures/Firespider.png",
+	},
+	fly: {
+		sourceString:"../InnerGame/res/sprites/creatures/Fly.png",
+	},
+	r0: {
+		sourceString:"../InnerGame/res/sprites/repeater/r0.png",
+	},
+	r1: {
+		sourceString:"../InnerGame/res/sprites/repeater/r1.png",
+	},
+	r2: {
+		sourceString:"../InnerGame/res/sprites/repeater/r2.png",
+	},
+	r3: {
+		sourceString:"../InnerGame/res/sprites/repeater/r3.png",
+	},
+	r4: {
+		sourceString:"../InnerGame/res/sprites/repeater/r4.png",
+	},
+	r5: {
+		sourceString:"../InnerGame/res/sprites/repeater/r5.png",
+	},
+	r6: {
+		sourceString:"../InnerGame/res/sprites/repeater/r6.png",
+	},
+	r7: {
+		sourceString:"../InnerGame/res/sprites/repeater/r7.png",
+	},
+	sn0: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0001.png",
+	},
+	sn0: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0001.png",
+	},
+	sn1: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0002.png",
+	},
+	sn2: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0003.png",
+	},
+	sn3: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0004.png",
+	},
+	sn4: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0005.png",
+	},
+	sn5: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0006.png",
+	},
+	sn6: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0007.png",
+	},
+	sn7: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0008.png",
+	},
+	sn8: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0009.png",
+	},
+	sn9: {
+		sourceString:"../InnerGame/res/sprites/sniper/s0010.png",
+	},
+	t0: {
+		sourceString:"../InnerGame/res/sprites/t2/t0001.png",
+	},
+	t1: {
+		sourceString:"../InnerGame/res/sprites/t2/t0002.png",
+	},
+	t2: {
+		sourceString:"../InnerGame/res/sprites/t2/t0003.png",
+	},
+	t3: {
+		sourceString:"../InnerGame/res/sprites/t2/t0004.png",
+	},
+	t4: {
+		sourceString:"../InnerGame/res/sprites/t2/t0005.png",
+	},
+	t5: {
+		sourceString:"../InnerGame/res/sprites/t2/t0006.png",
+	},
+	t6: {
+		sourceString:"../InnerGame/res/sprites/t2/t0007.png",
+	},
+	t7: {
+		sourceString:"../InnerGame/res/sprites/t2/t0008.png",
+	},
+	t8: {
+		sourceString:"../InnerGame/res/sprites/t2/t0009.png",
+	},
+	t9: {
+		sourceString:"../InnerGame/res/sprites/t2/t0010.png",
+	},
+	t10: {
+		sourceString:"../InnerGame/res/sprites/t2/t0011.png",
+	},
+	t11: {
+		sourceString:"../InnerGame/res/sprites/t2/t0012.png",
+	},
+	t12: {
+		sourceString:"../InnerGame/res/sprites/t2/t0013.png",
+	},
+	t13: {
+		sourceString:"../InnerGame/res/sprites/t2/t0014.png",
+	},
+	t14: {
+		sourceString:"../InnerGame/res/sprites/t2/t0015.png",
+	},
+	t15: {
+		sourceString:"../InnerGame/res/sprites/t2/t0016.png",
+	},
+	t16: {
+		sourceString:"../InnerGame/res/sprites/t2/t0017.png",
+	},
+	t17: {
+		sourceString:"../InnerGame/res/sprites/t2/t0018.png",
+	},
+	t18: {
+		sourceString:"../InnerGame/res/sprites/t2/t0019.png",
+	},
+	t19: {
+		sourceString:"../InnerGame/res/sprites/t2/t0020.png",
+	},
+
 	// Trading post images
 	shopBackground: {
 		sourceString:"shopIMGS/background.png",
@@ -133,136 +266,127 @@ function loadImages(){
 		loadSounds();
 	});
 }
-// let imageSources = {//Properties of imageSources match names of images
-// 	//outer game images
-// 	Asteroid1: {
-// 		sourceString:"res/sprites/Planet/Asteroid1.png",
-// 	},
-// 	Asteroid2: {
-// 		sourceString:"res/sprites/Planet/Asteroid2.png",
-// 	},
-// 	Asteroid3: {
-// 		sourceString:"res/sprites/Planet/Asteroid3.png",
-// 	},
-// 	Asteroid4: {
-// 		sourceString:"res/sprites/Planet/Asteroid4.png",
-// 	},
-// 	Asteroid5: {
-// 		sourceString:"res/sprites/Planet/Asteroid5.png",
-// 	},
-// 	Asteroid6: {
-// 		sourceString:"res/sprites/Planet/Asteroid6.png",
-// 	},
-// 	Asteroid7: {
-// 		sourceString:"res/sprites/Planet/Asteroid7.png",
-// 	},
-// 	Asteroid8: {
-// 		sourceString:"res/sprites/Planet/Asteroid8.png",
-// 	},
-// 	Asteroid9: {
-// 		sourceString:"res/sprites/Planet/Asteroid9.png",
-// 	},
-// 	Asteroid10: {
-// 		sourceString:"res/sprites/Planet/Asteroid10.png",
-// 	},
-// 	Planet1: {
-// 		sourceString:"res/sprites/Planet/PlanetTestRender0001.png",
-// 	},
-// 	Planet2: {
-// 		sourceString:"res/sprites/Planet/PlanetTestRender0002.png",
-// 	},
-// 	Planet3: {
-// 		sourceString:"res/sprites/Planet/PlanetTestRender0003.png",
-// 	},
-// 	Planet4: {
-// 		sourceString:"res/sprites/Planet/PlanetTestRender0004.png",
-// 	},
-// 	Planet5: {
-// 		sourceString:"res/sprites/Planet/PlanetTestRender0005.png",
-// 	},
-// 	enemyShip: {
-// 		sourceString:"res/ships/enemyShip.png",
-// 	},
-// 	playerShip: {
-// 		sourceString:"res/ships/playerShip.png",
-// 	},
-// 	//inner game images
-// 	grass: {
-// 		sourceString:"../InnerGame/res/sprites/grass.png",
-// 	},
-// 	fog: {
-// 		sourceString:"../InnerGame/res/sprites/fog.png",
-// 	},
-// 	rocks2c: {
-// 		sourceString:"../InnerGame/res/sprites/rocks2c.png",
-// 	},
-// 	tractor: {
-// 		sourceString:"../InnerGame/res/sprites/tractor.png",
-// 	},
-// 	WATERTiles: {
-// 		sourceString:"../InnerGame/res/sprites/WATERTiles.png",
-// 	},
-// 	hpPU: {
-// 		sourceString:"../InnerGame/res/sprites/hpPU.png",
-// 	},
-// 	shieldPU: {
-// 		sourceString:"../InnerGame/res/sprites/shieldPU.png",
-// 	},
-// 	moneyPU: {
-// 		sourceString:"../InnerGame/res/sprites/moneyPU.png",
-// 	},
-// 	techPU: {
-// 		sourceString:"../InnerGame/res/sprites/techPU.png",
-// 	},
-// 	arrowPU: {
-// 		sourceString:"../InnerGame/res/sprites/arrowPU.png",
-// 	},
-// 	splash: {
-// 		sourceString:"../InnerGame/res/sprites/splash/youdied.jpg",
-// 	},
-// 	// Trading post images
-// 	shopBackground: {
-// 		sourceString:"shopIMGS/background.png",
-// 	},
-// 	brownie: {
-// 		sourceString:"shopIMGS/brownie.png",
-// 	},
-// 	shopButton: {
-// 		sourceString:"shopIMGS/button.png",
-// 	},
-// 	cake: {
-// 		sourceString:"shopIMGS/cake.png",
-// 	},
-// 	coffee: {
-// 		sourceString:"shopIMGS/coffee.png",
-// 	},
-// 	cookie: {
-// 		sourceString:"shopIMGS/cookie.png",
-// 	},
-// 	cupcake: {
-// 		sourceString:"shopIMGS/cupcake.png",
-// 	},
-// }
+
+let soundSources = {//Properties of soundSources match names of sounds
+	backgroundMusic: {
+		sourceString:"res/sounds/A/bgS.mp3",
+	},
+	backgroundMusicAlt: {
+		sourceString:"res/sounds/B/bgS.mp3",
+	},
+	badNoiseDontUse: {
+		sourceString:"res/sounds/B/GoodNoiseplsuse.wav",
+	},
+	click: {
+		sourceString:"res/sounds/A/click.mp3",
+	},
+	cllsn: {
+		sourceString:"res/sounds/A/cllsn.mp3",
+	},
+	ding: {
+		sourceString:"res/sounds/A/ding.mp3",
+	},
+	doom: {
+		sourceString:"res/sounds/A/doom.mp3",
+	},
+	error: {
+		sourceString:"res/sounds/B/err.wav",
+	},
+	keyStroke1: {
+		sourceString:"res/sounds/B/computernoise1.wav",
+	},
+	keyStroke2: {
+		sourceString:"res/sounds/B/computernoise2.wav",
+	},
+	keyStroke3: {
+		sourceString:"res/sounds/B/computernoise3.wav",
+	},
+	mchn: {
+		sourceString:"res/sounds/A/mchn.mp3",
+	},
+	money: {
+		sourceString:"res/sounds/A/money.mp3",
+	},
+	point: {
+		sourceString:"res/sounds/B/points.wav",
+	},
+	mS1: {
+		sourceString:"res/sounds/A/mS1.mp3",
+	},
+	shipCollide1: {
+		sourceString:"res/sounds/B/CollidingwithOtherShips1.wav",
+	},
+	shipCollide2: {
+		sourceString:"res/sounds/B/CollidingwithOtherShips3.wav",
+	},
+	shipForwards: {
+		sourceString:"res/sounds/B/ShipNoiseForward.wav",
+	},
+	shipLand: {
+		sourceString:"res/sounds/B/SpaceShipLanding1.wav",
+	},
+	smash: {
+		sourceString:"res/sounds/A/smash.mp3",
+	},
+	spaceSounds: {
+		sourceString:"res/sounds/B/spaceambience1.wav",
+	},
+	takeoff: {
+		sourceString:"res/sounds/B/Takeoff.wav",
+	},
+	tDest: {
+		sourceString:"res/sounds/A/tDest.mp3",
+	},
+	tNoise: {
+		sourceString:"res/sounds/A/tNoise.mp3",
+	},
+	tpLand: {
+		sourceString:"res/sounds/B/LandingonTradePost.wav",
+	},
+	tradeErr: {
+		sourceString:"res/sounds/B/unsuccessfultrade.wav",
+	},
+	uPup: {
+		sourceString:"res/sounds/A/uPup.mp3",
+	},
+	water: {
+		sourceString:"res/sounds/A/water.mp3",
+	},
+	weaponPulse: {
+		sourceString:"res/sounds/A/weapon.mp3",
+	},
+	xplsn1: {
+		sourceString:"res/sounds/B/explosion4.wav",
+	},
+	xplsn2: {
+		sourceString:"res/sounds/B/explosion5.wav",
+	},
+	xplsn3: {
+		sourceString:"res/sounds/B/explosion3.wav",
+	},
+
+
+}
 
   function loadSounds(){
-// 	let promises = [];
-// 	for(let index in soundSources){
-// 		if(imageSources.hasOwnProperty(index)){
-// 			promises.push(new Promise(function(resolve,reject){
-// 				var sou = new Audio(index);
-// 				img.addEventListener('load', function(){
-// 					resolve(img);
-// 				});
-// 				img.addEventListener('error', function(){
-// 					reject();
-// 				});
-// 				img.src = imageSources[index].sourceString;
-// 			}).then(function(img){
-// 				Sounds[index] = sou;
-// 			}))
-// 		}
-// 	}
-	Promise.all([]).then(function(){
+	let promises = [];
+	for(let index in soundSources){
+		if(soundSources.hasOwnProperty(index)){
+			promises.push(new Promise(function(resolve,reject){
+				var sou = new Audio(index);
+				sou.addEventListener('load', function(){
+					resolve(img);
+				});
+				sou.addEventListener('error', function(){
+					reject();
+				});
+				sou.src = soundSources[index].sourceString;
+			}).then(function(img){
+				Sounds[index] = sou;
+			}))
+		}
+	}
+	Promise.all(promises).then(function(){
 		init();
 	});
 }
