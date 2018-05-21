967c9ce7e6c8ba085517702376792e3acf4c480d
claytonwramsey <claytonwramsey@gmail.com>
2018-05-21 11:53:51 -0700

    the tower do an img and a pointy

--- public-html/InnerGame/res/js/map/tower/tower.js
+++ public-html/InnerGame/res/js/map/tower/tower.js
@@ -16,26 +16,35 @@ class Tower extends Updateable {
                 this.size = config.tile_size;
                 this.hp = this.maxhp;
                 game.mapManager.map[this.cloc.x][this.cloc.y].isOccupied = true;
+                this.dir = 0;
         }
         init(){
                 this.image = Images[this.type.imageid];
-                console.log(this.type);
         }
         update() {
-                if(!this.image){
-                        this.image = Images[this.type.imageid];
-                }
         }
         render() {
                 if (this.image) {
+                        let toPlayer = this.game.player.loc.duplicate();
+                        toPlayer.subtract(this.loc);
+                        this.game.context.save();
+                        this.game.context.translate(
+                                this.loc.x+config.tile_size/2,
+                                this.loc.y+config.tile_size/2
+                        )
+                        if(toPlayer.m<=this.type.range){
+                                this.dir=toPlayer.th;
+                        }
+                        this.game.context.rotate(toPlayer.th);
                         this.game.context.drawImage(
                                 this.image,
-                                this.loc.x - config.tile_size / 2,
-                                this.loc.y - config.tile_size / 2,
+                                - config.tile_size / 2,
+                                - config.tile_size / 2,
                                 config.tile_size,
                                 config.tile_size
                         );
-                } else {
+                        this.game.context.restore();
+                } else {//draw the debug circle
                         if (this.hp <= 0)
                                 return;
                         this.game.context.fillStyle = this.fillStyle;
