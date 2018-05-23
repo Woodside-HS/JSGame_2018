320699944b32476420a15b359885a9e174b893bb
SethReis <795091@seq.org>
2018-05-23 11:18:49 -0700

    still getting planets

--- public-html/OuterGame/js/planet.js
+++ public-html/OuterGame/js/planet.js
@@ -3,8 +3,9 @@ class Planet{
   constructor(radius, location){
     this.radius = radius;
     this.loc = location;
-    var planetImageNum = Math.floor(Math.random()*5+1);
+    var planetImageNum = Math.floor(Math.random()*11+1);
     this.planetImage = Images['Planet' + planetImageNum];
+    planetImageNum %= 5;
     this.storyImage = Images["panel0" + planetImageNum];
     this.storyPanelDiv = document.getElementById("gamepanel" + planetImageNum);
     this.storyPanelDiv.appendChild(this.storyImage);
