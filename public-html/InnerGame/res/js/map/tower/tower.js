0c391da2a61adca1db2a7c99536f4d1cfb54e3a4
claytonwramsey <claytonwramsey@gmail.com>
2018-05-23 10:56:35 -0700

    fixed text and circle location

--- public-html/InnerGame/res/js/map/tower/tower.js
+++ public-html/InnerGame/res/js/map/tower/tower.js
@@ -70,16 +70,16 @@ class Tower extends Updateable {
                         this.game.context.fillStyle = this.fillStyle;
                         this.game.context.beginPath();
                         this.game.context.arc(
-                                this.loc.x,
-                                this.loc.y,
-                                config.tile_size / 2,
+                                this.loc.x+config.tile_size/2,
+                                this.loc.y+config.tile_size/2,
+                                0.45*config.tile_size,
                                 0,
                                 2 * Math.PI);
                         this.game.context.fill();
                         this.game.context.fillStyle = this.fontstyle;
                         this.game.context.fillText(
                                 this.type.name,
-                                this.loc.x - config.tile_size / 2,
+                                this.loc.x,
                                 this.loc.y);
                         
                 }
